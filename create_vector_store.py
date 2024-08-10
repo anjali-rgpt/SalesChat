1b066aac03aebaf489ebc031abda9907241b995a
Anjali Ragupathi <anjur99@gmail.com>
2024-08-10 05:10:20 -0700

    Wrapped vector store creation into a function

--- create_vector_store.py
+++ create_vector_store.py
@@ -18,33 +18,30 @@ os.environ["APIFY_API_TOKEN"] = config.apify_token
 
 URL = "https://www.artisan.co"
 
-apify = ApifyWrapper()
+def create_index(url = URL):
+    apify = ApifyWrapper()
 
-loader = apify.call_actor(
-    actor_id = "apify/website-content-crawler",
-    run_input={"startUrls": [{"url": "https://www.artisan.co"}], "maxCrawlPages": 10, "crawlerType": "cheerio"},
-    dataset_mapping_function=lambda item: Document(
-        page_content=item["text"] or "", metadata={"source": item["url"]}
-    ),
-)
+    loader = apify.call_actor(
+        actor_id = "apify/website-content-crawler",
+        run_input={"startUrls": [{"url": "https://www.artisan.co"}], "maxCrawlPages": 10, "crawlerType": "cheerio"},
+        dataset_mapping_function=lambda item: Document(
+            page_content=item["text"] or "", metadata={"source": item["url"]}
+        ),
+    )
 
-home_page_dynamic = scrape_dynamic_content(URL)
+    home_page_dynamic = scrape_dynamic_content(URL)
 
-documents_all = loader.load() + home_page_dynamic
+    documents_all = loader.load() + home_page_dynamic
 
-text_splitter = RecursiveCharacterTextSplitter()
-documents = text_splitter.split_documents(documents_all)
-embeddings = OpenAIEmbeddings(model = "text-embedding-3-small")
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(documents_all)
+    embeddings = OpenAIEmbeddings(model = "text-embedding-3-small")
 
-vectorstore = FAISS.from_documents(documents, embeddings)
+    vectorstore = FAISS.from_documents(documents, embeddings)
 
-vectorstore.save_local("vector_store")
+    vectorstore.save_local("vector_store")
 
-print("Saved")
+    print("Saved")
 
-# query = "I need some help with setting up my campaign."
-# result = index.query_with_sources(query, llm=OpenAI())
 
-# print("answer:", result["answer"])
-# print("source:", result["sources"])
 
